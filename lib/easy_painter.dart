0677cca8181e0fc233d13c3feefa7912b1bbb4c1
Adam Fuller <alfuller16@gmail.com>
2019-07-07 13:15:58 -0500

    removed unused customPaint method

--- lib/easy_painter.dart
+++ lib/easy_painter.dart
@@ -14,16 +14,6 @@ class EasyPainter extends CustomPainter {
     @required this.animation,
   });
 
-  /// Return a CustomPaint widget which is a [size]x[size] square
-  static CustomPaint customPaint(Function(Canvas, Size, double) onPaint, double size, Animation<double> animation) {
-    return CustomPaint(
-      child: SizedBox.fromSize(
-        size: Size.square(size),
-      ),
-      painter: EasyPainter(onPaint: (_, __, ___) => onPaint(_, __, ___), size: size, animation: animation),
-    );
-  }
-
   @override
   void paint(Canvas canvas, Size size) => this.onPaint(canvas, size, animation.value);
 
