f427be978c5f990967cce8ac79a6a768e2932461
Adam Fuller <alfuller16@gmail.com>
2020-02-09 01:25:23 -0600

    added pointer listener to detect touch events

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
-## [0.0.1] - 07/07/2019
+## [0.1.0] - 9 Feb 2020
+* added `onPointerDown`, `onPointerUp`, and `onPointerMove` methods
 
-Initial release
+
+## [0.0.1] - 7 July 2019 - Initial release
 * CustomWidget - StatefulWidget which continuously repaints using a provided onPaint function
 * EasyPainter - CustomPainter wrapper that can produce a CustomPaint widget
--- lib/custom_widget.dart
+++ lib/custom_widget.dart
@@ -7,18 +7,48 @@ part 'easy_painter.dart';
 class CustomWidget extends StatefulWidget {
   /// Method where painting should be done
   final Function(Canvas, Size, double) onPaint;
+
   /// Height and width of the widget if null, this expands to fill parent
   final double size;
+
   /// Length of time for the widget to reanimate
   final Duration duration;
+
   /// Does the widget constantly repaint
   final bool doesRepaint;
+
+  /// Callback when the user presses down on the widget
+  ///
+  /// Calculated considering the top left corner of the CustomWidget being 0,0
+  /// and the bottom right corner being `size.width`,`size.height`
+  ///
+  /// onPressDown(xPosition,yPosition)
+  final void Function(double, double) onPointerDown;
+
+  /// Callback when the user releases their press on the widget
+  ///
+  /// Calculated considering the top left corner of the CustomWidget being 0,0
+  /// and the bottom right corner being `size.width`,`size.height`
+  ///
+  /// onPressRelease(xPosition,yPosition)
+  final void Function(double, double) onPointerUp;
+
+  /// Callback when the user drags their finger/cursor after a press down
+  ///
+  /// Calculated considering the top left corner of the CustomWidget being 0,0
+  /// and the bottom right corner being `size.width`,`size.height`
+  ///
+  /// onDrag(xPosition,yPosition)
+  final void Function(double, double) onPointerMove;
 
   CustomWidget({
     @required this.onPaint,
     this.size,
     this.duration = const Duration(seconds: 2),
     this.doesRepaint = true,
+    this.onPointerDown,
+    this.onPointerUp,
+    this.onPointerMove,
   });
 
   @override
@@ -28,6 +58,7 @@ class CustomWidget extends StatefulWidget {
 class _CustomWidgetState extends State<CustomWidget> with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> _animation;
+  GlobalKey _key = GlobalKey();
 
   _CustomWidgetState();
 
@@ -56,16 +87,41 @@ class _CustomWidgetState extends State<CustomWidget> with SingleTickerProviderSt
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      child: widget.size != null ? SizedBox.fromSize(
-        size: Size.square(widget.size),
-      ): SizedBox.expand(),
-      painter: EasyPainter(
-        onPaint: widget.onPaint ?? () {},
-        size: widget.size,
-        animation: _animation,
+    return Listener(
+      onPointerMove: widget.onPointerMove != null
+          ? (PointerEvent event) {
+              RenderBox box = _key.currentContext.findRenderObject();
+              Offset offset = box.globalToLocal(event.position);
+              widget.onPointerMove(offset.dx, offset.dy);
+            }
+          : null,
+      onPointerDown: widget.onPointerDown != null
+          ? (PointerEvent event) {
+              RenderBox box = _key.currentContext.findRenderObject();
+              Offset offset = box.globalToLocal(event.position);
+              widget.onPointerDown(offset.dx, offset.dy);
+            }
+          : null,
+      onPointerUp: widget.onPointerUp != null
+          ? (PointerEvent event) {
+              RenderBox box = _key.currentContext.findRenderObject();
+              Offset offset = box.globalToLocal(event.position);
+              widget.onPointerUp(offset.dx, offset.dy);
+            }
+          : null,
+      child: CustomPaint(
+        key: _key,
+        child: widget.size != null
+            ? SizedBox.fromSize(
+                size: Size.square(widget.size),
+              )
+            : SizedBox.expand(),
+        painter: EasyPainter(
+          onPaint: widget.onPaint ?? () {},
+          size: widget.size,
+          animation: _animation,
+        ),
       ),
     );
   }
 }
-
